ec6f40c31ce12e67f0a683aec005b1b50d865909
jeffmacdonald7 <jeffmacdonald@gmail.com>
2024-03-13 21:17:33 -0400

    uuid

--- scripts/install.py
+++ scripts/install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import subprocess
 
 config_file="/etc/purplecat/purplecat.conf"
 config_path="/etc/purplecat/"
@@ -11,15 +12,19 @@ def check_for_config_file():
       for line in file.readlines():
         print(line)
   except FileNotFoundError:
-        print("Error: " + config_file + " not found "  )
-        return
+        print("no  " + config_file + "assuming new install ")
+        create_new_config_file()
   except PermissionError:
         print ("Error: cant open " + config_file + " for reading ")
 
 def create_new_config_file():
-   print ("here ")
+  # Run the uuidgen command and capture its output
+   uuid_proc = subprocess.run(['uuidgen'], capture_output=True, text=True)
+   uuid=uuid_proc.stdout.strip()
+   print("uuid is "+ uuid )
 
 
 if __name__ == "__main__":
     check_for_config_file()
+
 
